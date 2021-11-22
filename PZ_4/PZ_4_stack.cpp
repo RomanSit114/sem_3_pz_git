ea139c42ad041622d01bcaf4777452d2e0d64fe4
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2021-11-22 13:01:20 +0300

    Update PZ_4_stack.cpp

--- PZ_4/PZ_4_stack.cpp
+++ PZ_4/PZ_4_stack.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <string>//чтобы работать со строками
-#include <set>//для хранения открытых узлов(через которые прошли)
-#include <sstream>//работа со строками, чтобы взять данные с cout
-#include <stack>//работа со stack, хранение нераскрытых вершин
+#include <string>
+#include <set>
+#include <sstream>
+#include <stack>
 using namespace std;
 
 int main() {
@@ -11,36 +11,42 @@ int main() {
 	cout << "Задание 3 stack\n\nВведите кол-во узлов:\n";
 	int N = 0;
 	cin >> N;
-	vector <vector<int>> Nodes(N); //двумерный вектор для хранения узлов
-	string stringValue;//будем хранить введенные числа
+	vector <vector<int>> Nodes(N); 
+	string stringValue;
 	getline(cin, stringValue);
-	for (int i = 0; i < N; i++) {//Для считывания данных с каждого узла
-		getline(cin, stringValue);//считывание перемнных из консоли в stringValue
-		for (int j = 0; j < N; j++) {//для обнуления вектора
+	for (int i = 0; i < N; i++) {
+		getline(cin, stringValue);
+		for (int j = 0; j < N; j++) {
 			Nodes[i].push_back(0);
 		}
-		istringstream stringNum(stringValue);//из строки в числа
+		istringstream stringNum(stringValue);
 		int connectNodes;//для считывания связи между узлами
-		cout << endl;  
-		while (stringNum >> connectNodes) {//для того, чтобы считать connectNodes из stringNum
-			cout << connectNodes;
-			cout << " ";
+
+		while (stringNum >> connectNodes) {
 			if (connectNodes != 0) {
 				Nodes[i][connectNodes - 1] = 1;
 			}
 		}
 	}
 	cout << endl;
+	cout << "Выводим таблицу связей " << endl;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cout << Nodes[i][j] << " ";
 		}
 		cout << endl;
 	}
-	vector <vector<int>> results;
-	set <int> resultOneNode;
+	cout << endl;
+	vector <vector<int>> result(N);
 	stack <int> x;
 	for (int i = 0; i < N; i++) {
+		set <int> resultOneNode = {};
+
+		for (int j = 0; j < N; j++)
+		{
+			result[i].push_back(0); 
+		}
+
 		x.push(i);
 		while (!x.empty()) {
 			int temp = x.top();
@@ -52,5 +58,19 @@ int main() {
 				}
 			}
 		}
+		for (auto it = resultOneNode.begin(); it != resultOneNode.end(); it++)
+		{
+			result[i][*it] = 1; 
+		}
+	}
+
+	cout << "Вывод матрицы достижимости" << endl;
+	for (auto el : result)
+	{
+		for (auto em : el)
+		{
+			cout << em << " ";
+		}
+		cout << endl;
 	}
 }
